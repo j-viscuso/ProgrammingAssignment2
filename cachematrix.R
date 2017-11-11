47a2d42c8e63ca2deb811538acb470fac1251573
Jérémie VISCUSO <J.VISCUSO@directassurance-fr.intraxa>
2017-11-11 16:01:52 +0100

    File committed cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,32 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    inv<-NULL
+    set <- function(y){
+        x<<-y
+        inv<<-NULL
+    }
+    get<-function() x
+    setinvert <-function(solve) inv<<- solve
+    getinvert<-function() inv
+    list(set=set, get=get,
+         setinvert=setinvert,
+         getinvert=getinvert)
 }
 
 
-## Write a short comment describing this function
 
+## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+  inv<-x$getinvert()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+data<-x$get()
+inv<-solve(data,...)
+x$setinvert(inv)
+inv
 }
